ad2ab15fec020b85f536caf144b94a10db620c66
Josh-Irelle <34638458+joshirelle@users.noreply.github.com>
2023-10-20 14:57:01 +0800

    new file 'to, paki-download then pakilagay sa facadmin_new folder

--- addterm.php
+++ addterm.php
@@ -3,19 +3,47 @@
 require_once "php/core.php";
 
 $sy = $_POST['acadYear'];
+$termID = $_POST['termID'];
+$isActive = $_POST['isActive'];
 
-$sql = "INSERT INTO school_year (sy) VALUES (:sy)";
+$sql = "SELECT sy, termID FROM school_year WHERE sy = :sy AND termID = :termID";
+$sql2 = "SELECT is_active FROM school_year WHERE is_active = 1";
+$sql3 = "INSERT INTO school_year (sy, termID, is_active) VALUES (:sy, :termid, :is_active)";
 
 // Begin transaction
 $pdo->beginTransaction();
 
 try {
-    $stmt_fac = $pdo->prepare($sql);
-    $stmt_fac->bindParam(":sy", $sy);
-    $stmt_fac->execute();
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(":sy", $sy);
+    $stmt->bindParam(":termID", $termID);
+    $stmt->execute();
+
+    $countSYTerm = $stmt->rowCount();
+
+    if ($countSYTerm == 0) {
+        $stmt = $pdo->prepare($sql2);
+        $stmt->execute();
+
+        $countIsActive = $stmt->rowCount();
+
+        if($countIsActive == 0) {
+            $stmt = $pdo->prepare($sql3);
+            $stmt->bindParam(":sy", $sy);
+            $stmt->bindParam(":termid", $termID);
+            $stmt->bindParam(":is_active", $isActive);
+            $stmt->execute();
+            
+            $pdo->commit();
+            echo "success";
+        }else{
+            echo "There is an active academic year, unable to set current record to active";
+        }
+    }else{
+        echo "Academic Year and Semester already exist, please try again";
+    }
+
     
-    $pdo->commit();
-    echo "success";
 } catch (PDOException $ex) {
     // prevent insert in database when error occurs
     $pdo->rollBack();
